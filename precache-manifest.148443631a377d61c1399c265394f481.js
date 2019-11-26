09b1c271b0756f047ef924cc9a7ceb865effedda
Arthur <arthurd800@wp.pl>
2019-11-26 18:57:56 +0100

    Updates

---
+++ precache-manifest.148443631a377d61c1399c265394f481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b05a7578710381cf027a61387c26f86",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8531c9e0c61938ae8db1",
+    "url": "/monsters-rolodex/static/css/main.b1ddbd60.chunk.css"
+  },
+  {
+    "revision": "4f813702f88edd3d6fbb",
+    "url": "/monsters-rolodex/static/js/2.1d36bae1.chunk.js"
+  },
+  {
+    "revision": "8531c9e0c61938ae8db1",
+    "url": "/monsters-rolodex/static/js/main.63df1c84.chunk.js"
+  },
+  {
+    "revision": "dff35058851d5b37bc9e",
+    "url": "/monsters-rolodex/static/js/runtime-main.374b8c12.js"
+  }
+]);
